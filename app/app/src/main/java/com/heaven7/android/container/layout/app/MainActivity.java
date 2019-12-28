7df7d95dd1e7d155b7ffd822b41bf704e0ddceaa
heaven7 <18111646505@163.com>
2019-12-28 18:51:55 +0800

    opt

--- app/app/src/main/java/com/heaven7/android/container/layout/app/MainActivity.java
+++ app/app/src/main/java/com/heaven7/android/container/layout/app/MainActivity.java
@@ -1,8 +1,9 @@
 package com.heaven7.android.container.layout.app;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, TestContainerLayoutActivity.class));
     }
 }
